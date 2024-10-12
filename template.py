bf2e9d2bd3f5ff3e2bc9cb1990a0b66b5af97a3c
fahim- hossen <fahim.fahim3408@gmail.com>
2024-10-13 02:02:22 +0600

    first

---
+++ template.py
@@ -0,0 +1,44 @@
+import os 
+from pathlib import Path 
+
+
+print(Path("F:\Vector Database\PineCone VectorDB ALL\ attention all you need"))  
+
+list_of_files = [
+
+    '.github/workflows/.gitkeep',
+    'src/__init__.py', 
+    'src/components/data_ingestion.py' , 
+    'src/components/data_transformation.py' , 
+    'src/components/model_trainer.py' ,  
+    'src/components/model_monitering.py' , 
+    'src/components/model_evaluation.py' ,  
+    'src/pipeline/__init__.py' ,  
+    'src/pipeline/training_pipeline.py' ,
+    'src/pipeline/prediction_pipeline.py' ,  
+    'src/utils/utils.py' , 
+    'tests/unit/__init___.py' , 
+    'tests/integration/__init__.py' ,
+    'init_setup.sh' , 
+    'requirements.txt' ,  
+    'experiment/experiments.ipynb' , 
+    'setup.py' , 
+    'setup.cfg' , 
+    'pyproject.toml' ,
+    'tox.ini' , 
+    
+
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath) 
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            
